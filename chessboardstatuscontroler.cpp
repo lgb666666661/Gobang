7679987919f83c955319e85e962a16a521287f58
lgb666666661 <1657948739@qq.com>
2023-03-21 23:29:12 +0800

    init chessboardstatuscontroler

---
+++ chessboardstatuscontroler.cpp
@@ -0,0 +1,6 @@
+#include "chessboardstatuscontroler.h"
+
+ChessboardStatusControler::ChessboardStatusControler()
+{
+
+}
---
+++ chessboardstatuscontroler.h
@@ -0,0 +1,12 @@
+#ifndef CHESSBOARDSTATUSCONTROLER_H
+#define CHESSBOARDSTATUSCONTROLER_H
+
+#include <QObject>
+
+class ChessboardStatusControler
+{
+public:
+    ChessboardStatusControler();
+};
+
+#endif // CHESSBOARDSTATUSCONTROLER_H
